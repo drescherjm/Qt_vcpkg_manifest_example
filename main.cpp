73f2dcfb99013dcd34e33c3e7166853a20a18fa8
John M. Drescher <drescherjm@gmail.com>
2025-07-14 19:38:56 -0400

    Begin to test using Qt5 or  Qt6
    Start developing the project such that we can select Qt5 or Qt6. At the moment this code assumes Qt5 since the current vcpkg.json tells vcpkg we want Qt5 however it's planned to make this a feature that the user can select.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QtGlobal>
+#include <QString>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    QPushButton button("Hello from Qt6!");
+    QPushButton button(QString("Hello from %1!").arg(QT_VERSION_STR));
     button.resize(200, 60);
     button.show();
     return app.exec();
